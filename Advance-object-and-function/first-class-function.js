7c0ea4547b9aba0d018d1fbd8079d3a46d050df6
ashutosh <ashumantoo@gmail.com>
2020-08-12 13:49:45 +0530

    first class functions

---
+++ Advance-object-and-function/first-class-function.js
@@ -0,0 +1,77 @@
+/*=======================================================================================
+ *                     FIRST CLASS FUNCTION
+ *   It means a function -
+ *               1. can be stored in a varible
+ *               2. can be passed as arguments to fucntions
+ *               3. can be returned by functions
+ *               4. can be stored in some data structures
+ *               5. can be hold their own property and methods
+ *
+ * ======================================================================================*/
+
+/**======================================================================================
+ *                       Attribues of a first class function
+ *=======================================================================================*/
+// Function definition and invocation
+function speak(string) {
+    console.log(string);
+}
+speak("Hello");                     // logs "Hello"
+
+// Store in a variable
+var talk = speak;
+talk("Hi");                         // logs "Hi"
+
+// Pass as an argument to a function
+// Return from a function
+function functionReturner(fn) {
+    return fn;
+}
+var chat = functionReturner(talk);
+chat("Good Morning");               // logs "Good Morning"
+
+// Store in a data structure
+var myFuncs = [talk];
+myFuncs[0]("Good Afternoon");       // logs "Good Afternoon"
+
+// Owns properties
+talk.myProperty = "bananas";
+console.log(talk.myProperty);       // logs "bananas"
+
+
+/**======================================================================================
+  *                      Uses of a First-Class Function
+  *=======================================================================================*/
+
+
+// 1. Higher Order Functions (function takes function as argument)
+var myNums = [1, 2, 3, 4, 5];
+
+function doubleNum(num) {
+    return num * 2;
+}
+
+var doubledNums = myNums.map(function (num) {
+    return num * 2;
+});
+console.log(doubledNums);           // logs "[2, 4, 6, 8, 10]"
+
+var otherDoubledNums = myNums.map(doubleNum);
+console.log(otherDoubledNums);      // logs "[2, 4, 6, 8, 10]"
+
+
+// 2. Partial Function Application (function returns function)
+function applicator(fn, val) {
+    return function () {
+        fn(val);
+    };
+}
+
+function speak(string) {
+    console.log(string);
+}
+
+var sayHello = applicator(speak, "Hello");
+sayHello();                         // logs "Hello";
+var sayGoodBye = applicator(speak, "Good Bye, see you!");
+sayGoodBye();
