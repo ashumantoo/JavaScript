b91470b1db386cb52da0c005719ef0b2938e07cf
ashutosh <ashumantoo@gmail.com>
2020-08-31 13:30:02 +0530

    spread operator.

--- ES6/spread.operator.js
+++ ES6/spread.operator.js
@@ -2,7 +2,7 @@
 // Spread Operator
 
 function addFourAges(a, b, c, d) {
-    return a + b + c + d;
+  return a + b + c + d;
 }
 
 var sum1 = addFourAges(18, 20, 40, 50);
@@ -13,7 +13,9 @@ var ages = [18, 20, 40, 50];
 var sum2 = addFourAges.apply(null, ages);
 console.log(sum2);
 
-//ES6
+//==========================================================================
+//ES6 
+//spreat operator on array
 const sum3 = addFourAges(...ages);
 console.log(sum3);
 
@@ -26,4 +28,62 @@ const bigFamily1 = [...familySmith, 'Jonas', ...familyMiller];
 console.log(bigFamily);
 console.log(bigFamily1);
 
+const users = [
+  { name: "user1", email: 'user1@gmail.com', password: "pass123", gender: "Male" },
+  { name: "user2", email: 'user2@gmail.com', password: "pass123", gender: "Female" },
+  { name: "user3", email: 'user3@gmail.com', password: "pass123", gender: "Male" }
+];
 
+const newUser = {
+  name: "user3",
+  email: 'user3@gmail.com',
+  password: "pass123",
+  gender: "Male"
+};
+
+const newUsers = [...users, {...newUser}];
+
+console.log('users-->', users);
+console.log('newUser-->', newUser);
+console.log('newUsers-->', newUsers);
+
+
+//=========================================================================
+//spread operator on object
+const celebrity = {
+  name: "Tom Cruise",
+  occupation: "actor",
+};
+
+const celebrityInfo = {
+  age: 45,
+  country: "USA"
+};
+
+const combinedObj = { ...celebrity, ...celebrityInfo };
+
+// console.log('celebrity -->', celebrity);
+// console.log('celebrityInfo -->', celebrityInfo);
+// console.log('combinedCelebrity -->', combinedObj);
+
+const changeCelebrityInfo = {
+  ...celebrityInfo,
+  age: 50,
+  country: "America"
+}
+
+const newCelebrityInfo = {
+  age: 48,
+  country: "Uniated state of America"
+}
+
+const modifyObjImmutableWay = {
+  ...celebrityInfo,
+  ...newCelebrityInfo
+}
+
+console.log('celebrity -->', celebrity);
+console.log('celebrityInfo -->', celebrityInfo);
+console.log('changeCelebrityInfo -->', changeCelebrityInfo);
+console.log('Modify Object Immutable way -->', modifyObjImmutableWay);
+console.log('combinedCelebrity -->', combinedObj);
