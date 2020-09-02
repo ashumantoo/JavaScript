1ff310e2bbe30d4dda11a59d564486ff76b86bdb
ashutosh <ashumantoo@gmail.com>
2020-09-02 12:43:29 +0530

    updated Asynchronous Javascript

--- Asynchronous-Javascript-callbacks-promise-asyn-await/callbacks.js
+++ Asynchronous-Javascript-callbacks-promise-asyn-await/callbacks.js
@@ -1,16 +1,74 @@
 function getRecipe() {
-    setTimeout(() => {
-        const recipeId = [523, 883, 432, 974];
-        console.log(recipeId);
-        setTimeout(id => {
-            const receipe = { title: 'Fresh tomato pasta', publisher: 'Jonas' };
-            console.log(`${id} : ${receipe.title}`);
-            setTimeout((publisher) => {
-                const receipe2 = { title: 'Italian Pizza', publisher: 'Jonas' };
-                console.log(receipe);
-            }, 1500, receipe.publisher);
-        }, 1500, recipeId[2]);
-    }, 1500);
+  setTimeout(() => {
+    const recipeId = [523, 883, 432, 974];
+    console.log(recipeId);
+    setTimeout(id => {
+      const receipe = { title: 'Fresh tomato pasta', publisher: 'Jonas' };
+      console.log(`${id} : ${receipe.title}`);
+      setTimeout((publisher) => {
+        const receipe2 = { title: 'Italian Pizza', publisher: 'Jonas' };
+        console.log(receipe);
+      }, 1500, receipe.publisher);
+    }, 1500, recipeId[2]);
+  }, 1500);
 }
 
 getRecipe();
+
+
+
+//=============================== 2nd Example ===================================
+console.log("start");
+
+/*
+const getUser = (email, password) => {
+  setTimeout(() => {
+    console.log('now we have data');
+    return { email, password }
+  }, 2000);
+}
+
+const user = getUser('ashu@me.com', 12234);
+console.log(user); // this line of code will execute before finishing the getUser(), which will be undefined 
+
+*/
+
+
+const getUser = (email, password, callback) => {
+  setTimeout(() => {
+    console.log('now we have data');
+    callback({ email, password });
+  }, 2000);
+};
+
+const getUserVideos = (email, callback) => {
+  if (!email) {
+    callback('Email not found');
+  } else {
+    setTimeout(() => {
+      callback(['video1', 'video2', 'video3', 'video4']);
+    }, 1500);
+  }
+};
+
+const getVideoDetails = (videoId, callback) => {
+  if (videoId) {
+    setTimeout(() => {
+      callback(`This is a sample video`);
+    }, 1000)
+  } else {
+    callback("Video Id not found");
+  }
+};
+
+const user = getUser('ashu@me.com', 12234, (user) => {
+  console.log(user);
+  getUserVideos(user.email, (videos) => {
+    console.log(videos);
+    getVideoDetails('1', (video) => {
+      console.log(video);
+    })
+  });
+});
+
+console.log('finish');
--- Asynchronous-Javascript-callbacks-promise-asyn-await/promise.js
+++ Asynchronous-Javascript-callbacks-promise-asyn-await/promise.js
@@ -1,35 +1,110 @@
 const getIDs = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve([234, 567, 896, 486]);
-    }, 1500);
+  setTimeout(() => {
+    resolve([234, 567, 896, 486]);
+  }, 1500);
 });
 
 const getRecipe = recId => {
-    return new Promise((resolve, reject) => {
-        setTimeout((ID) => {
-            const recipe = { title: 'Fresh tomato pasta', publisher: 'Jonas' };
-            resolve(`${ID} : ${recipe.title}`);
-        }, 1500, recId);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout((ID) => {
+      const recipe = { title: 'Fresh tomato pasta', publisher: 'Jonas' };
+      resolve(`${ID} : ${recipe.title}`);
+    }, 1500, recId);
+  });
 };
 
 const getRelated = publisher => {
-    return new Promise((resolve, reject) => {
-        setTimeout((pub) => {
-            const recipe = { title: 'Italian Pizza', publisher: 'Jonas' };
-            resolve(`${pub} : ${recipe.title}`);
-        }, 1500, publisher);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout((pub) => {
+      const recipe = { title: 'Italian Pizza', publisher: 'Jonas' };
+      resolve(`${pub} : ${recipe.title}`);
+    }, 1500, publisher);
+  });
 };
 
 getIDs.then(IDs => {
-    console.log(IDs);
-    return getRecipe(IDs[2]);
+  console.log(IDs);
+  return getRecipe(IDs[2]);
 }).then(recipe => {
-    console.log(recipe);
-    return getRelated('Jonas');
+  console.log(recipe);
+  return getRelated('Jonas');
 }).then((recipe) => {
-    console.log(recipe);
+  console.log(recipe);
 }).catch(error => {
+  console.log(error);
+});
+
+
+//====================================== Example 2============================
+
+const getPost = (pid) => {
+  return new Promise((resolve, reject) => {
+    if (!pid) {
+      reject('Post id not found');
+    } else {
+      setTimeout(() => {
+        resolve({
+          id: 1,
+          title: "First Post",
+          body: "Test Post",
+          author: "author1231",
+        });
+      }, 1500);
+    }
+  })
+};
+
+const getAuthor = (authorId) => {
+  return new Promise((resolve, reject) => {
+    if (!authorId) {
+      reject('Not a valid author Id');
+    } else {
+      setTimeout(() => {
+        resolve({
+          id: "1",
+          name: "Ashutosh Mantoo",
+          email: 'ashutosh@mail.com',
+          age: 27
+        });
+      }, 1000)
+    }
+  });
+};
+
+
+// const post = getPost('1');  //will gives a promise
+// console.log(post);
+getPost('1').then((post) => {
+  console.log(post);
+  getAuthor(post.author).then((author) => {
+    console.log(author);
+  }).catch(error => {
     console.log(error);
+  });
+}).catch((error) => {
+  console.log(error);
+});
+
+
+//================================
+const yt = new Promise((resolv, reject) => {
+  setTimeout(() => {
+    console.log('getting stuff from youtube');
+    resolve({ videos: [1, 2, 3, 4, 5, 6] })
+  }, 2000);
+});
+
+
+const fb = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log('stuff from fb');
+    resolve({ username: "ashumantoo" });
+  }, 3000);
+});
+
+//Promise all 
+Promise.all(yt, fb).then(result => {
+  console.log(result);
+}).catch((error) => {
+  console.log(error);
 });
